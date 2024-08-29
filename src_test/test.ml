3fcc1104ba7d3d02e96567a93322e28eb2856243
mefyl <mefyl@gruntech.org>
2024-08-29 11:58:56 +0200

    Add failing test.

--- src_test/test.ml
+++ src_test/test.ml
@@ -1,15 +1,6 @@
-let old_lines = [| "I"; "really"; "like"; "icecream" |]
-let new_lines = [| "I"; "do"; "not"; "like"; "icecream" |]
 
 module Diff = Simple_diff.Make(String)
 open Diff
-
-let expected =
-  [ Equal   [| "I"; |];
-    Deleted [| "really" |];
-    Added [| "do"; "not" |];
-    Equal [| "like"; "icecream" |];
-  ]
 
 let pp diffs =
   List.fold_left(fun str diff ->
@@ -26,9 +17,34 @@ let pp diffs =
   ) "" diffs
 
 let () =
-  let diffs = get_diff old_lines new_lines in
-  if diffs = expected then
-    Printf.printf "SUCCESS"
-  else
-    let () = Printf.printf "FAILED!\n\nActual:%s" (pp diffs) in
-    Printf.printf "\n\nExpected:%s" (pp expected)
+  let check old_lines new_lines expected =
+    let diffs = get_diff old_lines new_lines in
+    if diffs <> expected then
+      let () = Printf.printf "FAILED!\n\nActual:%s" (pp diffs) 
+      and () =      Printf.printf "\n\nExpected:%s" (pp expected) in
+      exit 1
+  in
+  let () =
+    let old_lines = [| "I"; "really"; "like"; "icecream" |]
+    and new_lines = [| "I"; "do"; "not"; "like"; "icecream" |]
+    and expected =
+      [ Equal   [| "I"; |];
+        Deleted [| "really" |];
+        Added [| "do"; "not" |];
+        Equal [| "like"; "icecream" |];
+      ]
+    in  check old_lines new_lines expected
+  and () =
+    let old_lines = [| "C"; "B"; "A" |]
+    and new_lines = [| "A"; "B"; "C" |]
+    and expected =
+      [ Deleted [| "C" |];
+        Added [| "A" |];
+        Equal [| "B" |];
+        Deleted [| "A" |];
+        Added [| "C" |];
+      ]
+    in  check old_lines new_lines expected
+  in
+  Printf.printf "SUCCESS"
+
